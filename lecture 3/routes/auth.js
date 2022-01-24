84e0e813500638dbdbc2a901ca4dce2716d95a2b
mernaamir <mernaamir88@gmail.com>
2022-01-24 21:42:27 +0200

    nodetask

---
+++ lecture 3/routes/auth.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { User } = require("../models/user");
+const bcrypt = require("bcrypt");
+const Joi = require("joi")
+const jwt = require("jsonwebtoken");
+
+const validate = (auth) => {
+    const schema = Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().required()
+    })
+    return schema.validate(auth)
+}
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).json({ message: error.details[0].message });
+
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) return res.status(401).json({ message: "email or password is incorrect" });
+
+    const isValidPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!isValidPassword) return res.status(401).json({ message: "email or password is incorrect" });
+
+    // res.json(true)
+
+    const token = jwt.sign({ _id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET)
+    res.json({ token })
+})
+
+module.exports = router
---
+++ lecture 3/try.js
@@ -0,0 +1,9 @@
+const _ = require("lodash");
+
+
+const r ={
+
+    id:1,
+    name:"asd"
+}
+console.log(_.omit(r,['id']));
